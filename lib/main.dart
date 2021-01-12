c8576cc8cb9a6ea9552c3574d0d3f2464fd54f72
Gustavo <luisgustavolg1033@gmail.com>
2021-01-12 20:59:41 -0300

    (feat) add search and results card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertube/api.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/screens/home.dart';
 
 void main() {
@@ -12,10 +14,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlutterTube',
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    );
+    return BlocProvider(
+        bloc: VideosBloc(),
+        child: MaterialApp(
+          title: 'FlutterTube',
+          home: Home(),
+          debugShowCheckedModeBanner: false,
+        ));
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,14 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/delegates/data_search.dart';
+import 'package:fluttertube/widgets/videotile.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<VideosBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Container(
@@ -12,7 +17,7 @@ class Home extends StatelessWidget {
         ),
         elevation: 0,
         backgroundColor: Colors.black87,
-        actions: [
+        actions: <Widget>[
           Align(
             alignment: Alignment.center,
             child: Text("0"),
@@ -27,11 +32,45 @@ class Home extends StatelessWidget {
               String result =
                   await showSearch(context: context, delegate: DataSearch());
               print("result: " + result);
+
+              if (result != null) bloc.inSearch.add(result);
             },
           ),
         ],
       ),
-      body: Container(),
+      backgroundColor: Colors.black87,
+      body: StreamBuilder(
+        stream: BlocProvider.of<VideosBloc>(context).outVideos,
+        initialData: [],
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                if (index < snapshot.data.length) {
+                  return VideoTile(snapshot.data[index]);
+                } else if (index > 1) {
+                  bloc.inSearch.add(null);
+                  return Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Colors.red,
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              },
+              itemCount: snapshot.data.length + 1,
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
     );
   }
 }
