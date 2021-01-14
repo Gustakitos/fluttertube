0f5bd0146a55e8fc03614c2d511dfa868ce91559
Gustavo <luisgustavolg1033@gmail.com>
2021-01-13 22:21:19 -0300

    saved favorite videos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertube/api.dart';
+import 'package:fluttertube/blocs/favorite_bloc.dart';
 import 'package:fluttertube/blocs/videos_bloc.dart';
 import 'package:fluttertube/screens/home.dart';
 
@@ -15,11 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        bloc: VideosBloc(),
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
         child: MaterialApp(
           title: 'FlutterTube',
           home: Home(),
           debugShowCheckedModeBanner: false,
-        ));
+        ),
+      ),
+    );
   }
 }
